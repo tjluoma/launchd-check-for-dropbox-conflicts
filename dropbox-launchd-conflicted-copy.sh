448fd19d10fb2f14fb4635feedf240d3b3f5ac61
TJ Luoma <luomat@gmail.com>
2012-08-02 14:41:11 -0400

    added comments to shell script

--- dropbox-launchd-conflicted-copy.sh
+++ dropbox-launchd-conflicted-copy.sh
@@ -11,16 +11,24 @@ NAME=`echo $0:t`
 	# change this if your Dropbox is somewhere else
 DIR=$HOME
 
+	# this will look for files with the name "'s conflicted copy YYYY-MM-DD" in it
+	# except this in the Trash or the .dropbox.cache folder.
+	# then it will count the lines (wc) and get rid of anything except the
+	# number (tr) since 'wc' inexplicably adds blank spaces before its output.
 COUNT=$(find "$DIR" -path "*(*'s conflicted copy [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]*" -print |\
 egrep -v "$DIR/.dropbox.cache|$HOME/.Trash/" | wc -l | tr -dc '[0-9]')
 
 if [[ "$SHLVL" == "2" ]]
 then
-		#
+		# if Shell Level is 2 then this is being run in iTerm or Terminal.
+		# SHLVL is 1 for launchd
+		# so we just output the information and then exit
 	echo "$NAME: Number of Dropbox 'conflicted copy' files found: $COUNT"
 	exit 0
 
 fi
+
+# if we get here, we are in launchd
 
 	# if no conflicted files, exit
 [[ "$COUNT" == "0" ]] && exit 0
@@ -34,4 +42,3 @@ fi
 exit 0
 
 # EOF
-
